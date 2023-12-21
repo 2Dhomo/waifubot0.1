b7a680d411a7b36b52afe2a181808e161c525fc2
DORAxNET <151729806+DORAxNET@users.noreply.github.com>
2023-12-21 19:57:35 +0530

    Update harem.py

--- Grabber/modules/harem.py
+++ Grabber/modules/harem.py
@@ -49,7 +49,7 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
         for character in characters:
             
             count = character_counts[character['id']]  # Get the count from the character_counts dictionary
-            harem_message += f'➥{character["id"]}| {character['rarity']} |{character["name"]} ×{count}\n'
+            harem_message += f'➥{character["id"]}| {character["rarity"]} |{character["name"]} ×{count}\n'
 
 
     total_count = len(user['characters'])
