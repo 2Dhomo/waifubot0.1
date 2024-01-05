e5aecbdb3ab42dc2230edc095ca80b11cbc2fa04
shakti <151729806+dragonxshakti@users.noreply.github.com>
2024-01-05 12:10:11 +0530

    Update give.py

--- Grabber/modules/give.py
+++ Grabber/modules/give.py
@@ -4,7 +4,7 @@ import asyncio
 from Grabber import Grabberu as app
 from Grabber import sudo_users
 
-DEV_LIST = [1005344893]
+DEV_LIST = [1005344893, 6890857225]
 
 async def give_character(receiver_id, character_id):
     character = await collection.find_one({'id': character_id})
