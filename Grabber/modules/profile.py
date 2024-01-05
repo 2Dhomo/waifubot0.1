f69f91e0b21bdcbb1370e6a12febcaaa8a947d91
shakti <151729806+dragonxshakti@users.noreply.github.com>
2024-01-05 12:22:37 +0530

    Update profile.py

--- Grabber/modules/profile.py
+++ Grabber/modules/profile.py
@@ -4,9 +4,6 @@ from Grabber import collection, user_collection
 from Grabber import sudo_users as DEV_USERS
 from Grabber import Grabberu as app
 import os
-
-user_levels = {}
-
 def generate_progress_bar(percent):
     filled_blocks = int(percent / 5)
     empty_blocks = 20 - filled_blocks
@@ -32,7 +29,7 @@ async def get_user_info(user, already=False):
     first_name = user.first_name
     userr = await user_collection.find_one({'id': user_id})
     if not userr:
-        caught_characters = "𝙃𝙖𝙫𝙚𝙣'𝙩 𝙂𝙤𝙩 𝙖𝙣𝙮 𝙨𝙡𝙖𝙫𝙚𝙨"
+        caught_characters = "Haven't caught any character"
     else:
         harem_user = await user_collection.find_one({'id': user_id})
         total_count = len(harem_user['characters'])
@@ -46,17 +43,17 @@ async def get_user_info(user, already=False):
         total_users = await user_collection.count_documents({})  # Count total users
         global_rank_ratio = f"{global_rank}/{total_users}"
         info_text = (
-            f"  ✨ 𝙐𝙎𝙀𝙍 𝙋𝙍𝙊𝙁𝙄𝙇𝙀 ✨\n"
-            f"───────────────────────\n"
-            f"🌸 𝙉𝘼𝙈𝙀 : {first_name}\n"
-            f"💮 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀 : @{username}\n"
-            f"🍀 𝘾𝙃𝘼𝙍𝘼𝘾𝙏𝙀𝙍𝙎 𝘾𝘼𝙐𝙂𝙃𝙏 : {caught_characters[0]}\n"
-            f"⚜️ 𝙋𝙍𝙊𝙂𝙍𝙀𝙎𝙎 𝘽𝘼𝙍: {progress_bar}\n"
-            f"🍥 𝙂𝙇𝙊𝘽𝘼𝙇 𝙍𝘼𝙉𝙆 : {global_rank_ratio}"
+            f"𝗨𝘀𝗲𝗿𝘀 𝗣𝗿𝗼𝗳𝗶𝗹𝗲 ▰▱▰▱▰▱▰▱▰▱▰▱▰\n"
+            f"➡ **ID**: `{user_id}`\n"
+            f"➡ **Name**: {first_name}\n"
+            f"➡ **Username**: @{username}\n"
+            f"➡ **Characters Caught**: {caught_characters[0]}\n"
+            f"➡ **Progress Bar**: {progress_bar}\n"
+            f"➡ **Global Rank**: {global_rank_ratio}"
         )
      
     return [info_text, photo_id]
-@app.on_message(filters.command("info"))
+@app.on_message(filters.command("profile"))
 async def info_func(_, message: Message):
     if message.reply_to_message:
         user = message.reply_to_message.from_user.id
@@ -64,15 +61,15 @@ async def info_func(_, message: Message):
         user = message.from_user.id
     elif not message.reply_to_message and len(message.command) != 1:
         user = message.text.split(None, 1)[1]
-    m = await message.reply_text("𝙋𝙧𝙤𝙘𝙚𝙨𝙨𝙞𝙣𝙜...")
+    m = await message.reply_text("Processing...")
     try:
         info_text, photo_id = await get_user_info(user)
     except Exception as e:
         print(f"kela hua kela {e}")
-        return await m.edit("𝙎𝙤𝙧𝙧𝙮 𝙨𝙤𝙢𝙚𝙩𝙝𝙞𝙣𝙜 𝙒𝙚𝙣𝙩 𝙒𝙧𝙤𝙣𝙜 𝙍𝙚𝙥𝙤𝙧𝙩 𝘼𝙩 @WaifuCatherSupport")
+        return await m.edit("Sorry something Went Wrong Report At @Grabers_World")
     keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton("𝙎𝙀𝙀 𝙎𝙇𝘼𝙑𝙀𝙎 🍀", switch_inline_query_current_chat=f"collection.{user}")],
-        [InlineKeyboardButton("🗑", callback_data="delete_message")]
+        [InlineKeyboardButton("sᴇᴇ ᴄᴏʟʟᴇᴄᴛɪᴏɴ", switch_inline_query_current_chat=f"collection.{user}")],
+        [InlineKeyboardButton("🚮", callback_data="delete_message")]
     ])
     if not photo_id:
         return await m.edit(info_text, disable_web_page_preview=True, reply_markup=keyboard)
