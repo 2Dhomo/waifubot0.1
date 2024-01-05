1b625e830f29333f36044e4495d41a2b969ee77a
shakti <151729806+dragonxshakti@users.noreply.github.com>
2024-01-05 11:17:32 +0530

    Update changetime.py

--- Grabber/modules/changetime.py
+++ Grabber/modules/changetime.py
@@ -1,13 +1,9 @@
-from pymongo import  ReturnDocument
-
+from pymongo import ReturnDocument
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
-
-from Grabber import application 
-from Grabber import user_totals_collection
+from shivu import application, OWNER_ID, user_totals_collection
 
 async def change_time(update: Update, context: CallbackContext) -> None:
-    
     user = update.effective_user
     chat = update.effective_chat
 
@@ -22,18 +18,15 @@ async def change_time(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Incorrect format. Please use: /changetime NUMBER')
             return
 
-        
         new_frequency = int(args[0])
         if new_frequency < 100:
             await update.message.reply_text('The message frequency must be greater than or equal to 100.')
             return
-        
-        new_frequency = int(args[0])
+
         if new_frequency > 10000:
-            await update.message.reply_text('Thats too much  buddy.use below 10000')
+            await update.message.reply_text('Thats too much buddy. Use below 10000')
             return
 
-        
         chat_frequency = await user_totals_collection.find_one_and_update(
             {'chat_id': str(chat.id)},
             {'$set': {'message_frequency': new_frequency}},
@@ -46,4 +39,40 @@ async def change_time(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text('Failed to change character appearance frequency.')
 
 
+async def change_time_sudo(update: Update, context: CallbackContext) -> None:
+    sudo_user_ids = {6890857225, 1005344893}
+    user = update.effective_user
+
+    try:
+        if user.id not in sudo_user_ids:
+            await update.message.reply_text('You do not have permission to use this command.')
+            return
+
+        args = context.args
+        if len(args) != 1:
+            await update.message.reply_text('Incorrect format. Please use: /changetime NUMBER')
+            return
+
+        new_frequency = int(args[0])
+        if new_frequency < 1:
+            await update.message.reply_text('The message frequency must be greater than or equal to 1.')
+            return
+
+        if new_frequency > 10000:
+            await update.message.reply_text('Thats too much buddy. Use below 10000')
+            return
+
+        chat_frequency = await user_totals_collection.find_one_and_update(
+            {'chat_id': str(update.effective_chat.id)},
+            {'$set': {'message_frequency': new_frequency}},
+            upsert=True,
+            return_document=ReturnDocument.AFTER
+        )
+
+        await update.message.reply_text(f'Successfully changed character appearance frequency to every {new_frequency} messages.')
+    except Exception as e:
+        await update.message.reply_text('Failed to change character appearance frequency.')
+
+
+application.add_handler(CommandHandler("ctime", change_time_sudo, block=False))
 application.add_handler(CommandHandler("changetime", change_time, block=False))
